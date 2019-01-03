eec111b4c32d407fc3393ce1106afd5e4a6a88d4
Leo Chan <cleojanten@hotmail.com>
2019-01-03 10:24:14 -0800

    function adjustments

--- src/job_runner.py
+++ src/job_runner.py
@@ -207,6 +207,9 @@ def run(ui_username, ui_password, ui_endpoints, ui_metrics, ui_tables):
             "whitelabels"
         ]
 
+    # Capturing total requests
+    n_th = 0
+
     for endpoint in ui_endpoints:
 
         if endpoint == "reviews" and last_update_time:
@@ -216,7 +219,7 @@ def run(ui_username, ui_password, ui_endpoints, ui_metrics, ui_tables):
                 del params["published_after"]
 
         logging.info("fetching endpoint {} ...".format(endpoint))
-        json_res = request_endpoint(ui_username, token, endpoint, params)
+        json_res = request_endpoint(ui_username, token, endpoint, params, n_th)
         if json_res == 404:
             logging.warning("Endpoint [{}] not found, 404 Error".format(endpoint))
             continue
@@ -230,8 +233,6 @@ def run(ui_username, ui_password, ui_endpoints, ui_metrics, ui_tables):
 
     metrics = _parse_ui_metrics(ui_metrics, account_id)
 
-    # Capturing total requests
-    n_th = 0
     for metric in metrics:
 
         params["month_before"] = metric.get("month_before")
