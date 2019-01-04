8968d655386e361c0ab9391fc6ded7667e85a874
Leo Chan <cleojanten@hotmail.com>
2019-01-04 11:32:14 -0800

    pagination fix

--- src/service/api_client.py
+++ src/service/api_client.py
@@ -22,6 +22,7 @@ def _build_headers(username, token):
 def request_endpoint(username, token, endpoint, params, n_th):
     entities = []
     headers = _build_headers(username, token)
+
     res = requests.get(url=BASE_URL + endpoint, headers=headers, params=params)
     if res.status_code == 404:
         print(res.text)
@@ -33,12 +34,13 @@ def request_endpoint(username, token, endpoint, params, n_th):
     else:
         # collect first page objects
         entities += res.get("_embedded").get(endpoint)
-        total_pages = res.get('_total_pages')
+        total_pages = int(res.get('_total_pages'))
         current_page_num = int(res.get('_page'))
         logging.info("Endpoint: [{0}]; Total Pages: [{1}]".format(endpoint, total_pages))
         # logging.info("Total Pages: [{}]".format(total_pages))
-        # logging.info("Current Page: [{}]".format(current_page_num))
+        logging.info("Current Page: [{0}] @ [{1}]".format(current_page_num, endpoint))
 
+        """
         while current_page_num < total_pages:
 
             entities_curr_page = res.get("_embedded").get(endpoint)
@@ -50,6 +52,23 @@ def request_endpoint(username, token, endpoint, params, n_th):
             params["page"] = current_page_num + 1
             res = requests.get(url=BASE_URL + endpoint, headers=headers, params=params)
             res = json.loads(res.text)
+        """
+        # while current_page_num < total_pages + 1:
+
+        logging.info("Current Page: [{0}] @ [{1}]".format(current_page_num, endpoint))
+        entities_curr_page = res.get("_embedded").get(endpoint)
+        entities += entities_curr_page
+
+        # next_url = res.get("_links").get("next").get("href")
+        # if "next" in res["_links"]:
+        while "next" in res["_links"]:
+            next_url = res["_links"]["next"]["href"]
+
+            res = requests.get(url=next_url, headers=headers, params=params)
+            res = json.loads(res.text)
+            current_page_num = int(res.get('_page'))
+            logging.info("Current Page: [{0}] @ [{1}]".format(current_page_num, endpoint))
+
 
         # Number of requests
         n_th = n_th + int(total_pages)
