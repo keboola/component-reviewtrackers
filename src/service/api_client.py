52d2f78b18588d08e8a38619f184511e1d11f1eb
Leo Chan <cleojanten@hotmail.com>
2019-01-09 11:19:14 -0800

    touch up

--- src/service/api_client.py
+++ src/service/api_client.py
@@ -61,7 +61,6 @@ def request_endpoint(username, token, state_file, endpoint, file_name, params):
         logging.info("Current Page: [{0}] @ [{1}] - Parsing".format(starting_page, endpoint))
         entities_curr_page = res.get("_embedded").get(endpoint)
         entities += entities_curr_page
-        print(res["_links"])
         parse(entities_curr_page, file_name)
         starting_page += 1
 
@@ -83,7 +82,6 @@ def request_endpoint(username, token, state_file, endpoint, file_name, params):
             """
             # if there are no more records, stop at that page
             if len(entities_curr_page) == 0:
-                print(entities_curr_page)
                 logging.info("No records found on page [{0}] @ [{1}]".format(starting_page, endpoint))
                 logging.info("Stopping [{0}] @ page [{1}]".format(endpoint, starting_page))
                 total_pages = starting_page
